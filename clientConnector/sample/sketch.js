3ee55317967ec67f8fd4368247b2cbfa1c5efa36
chrerv <tellart@MacBook-Pro.local>
2015-04-14 18:43:12 +0200

    sketch mods

--- clientConnector/sample/sketch.js
+++ clientConnector/sample/sketch.js
@@ -1,8 +1,11 @@
 var started=false;
 
+var myButton=-1;
+var circleSize=5;
+
 function setup() {
-   createCanvas(900, 900);
-   gem.getInstance().setupSocket();
+   createCanvas(windowWidth, windowHeight);
+   connector.getInstance().setupSocket("192.168.1.12:9092");
    frameRate(30); 
 }
 
@@ -12,13 +15,25 @@ function draw() {
 	fill(0, 102, 153, 51);
 	text("It works!", 10, 30); 
 	
- if(gem.getInstance().status=="OPENED" && !started)
+ if(connector.getInstance().status=="OPENED" && !started)
   {
-    console.log("registering accelerometer 1");
-   // gem.getInstance().registerAccelerometer(0);
+    console.log("registering button");
+    connector.getInstance().registerButton(0);
     started = true;
+    
+    connector.getInstance().makeVibrate(0);
   }
-  ellipse(100,100,clamp(map(gem.getInstance().accelerometer.y,-1024,1024,0,100),0,100),clamp(map(gem.getInstance().accelerometer.y,-1024,1024,0,100),0,100));
+
+  myButton = connector.getInstance().getButtonState();
+  if (myButton != -1) console.log("BUTTON!");
+  circleSize += myButton*5;
+
+  ellipse(100,100,circleSize,circleSize);
+
+  //ellipse(100,100,clamp(map(connector.getInstance().accelerometer.y,-1024,1024,0,100),0,100),clamp(map(connector.getInstance().accelerometer.y,-1024,1024,0,100),0,100));
 }
 
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
 
